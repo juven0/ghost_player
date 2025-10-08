6b653fe3ba6c9ccc28d785a9514bd278a5f6934f
juven0 <bocquelin@gmail.com>
2025-10-08 08:48:33 +0300

    add ytdlp for research methode

--- player/player.go
+++ player/player.go
@@ -1,7 +1,42 @@
 package player
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/lrstanley/go-ytdlp"
+)
+
+type Player struct {
+	ctx context.Context
+}
+
 type videoInfo struct {
 	Title    string `json:"title"`
 	Duration string `json:"duration"`
-	Url      string `json:"url"`
+	URL      string `json:"url"`
+}
+
+func SearchYoutube(query string, maxResult int) ([]videoInfo, error) {
+	ctx := context.Background()
+
+	dl := ytdlp.New().FlatPlaylist().DumpJSON()
+
+	searchQuery := fmt.Sprintf("ytsearch%d:%s", maxResult, query)
+	result, err := dl.Run(ctx, searchQuery)
+	if err != nil {
+		return []videoInfo{}, fmt.Errorf("search failed: %w", err)
+	}
+
+	var data struct {
+		Entries []videoInfo `json:"entries"`
+	}
+
+	err = json.Unmarshal([]byte(result.Stdout), &data)
+	if err != nil {
+		return []videoInfo{}, fmt.Errorf("error parse results: %w", err)
+	}
+
+	return data.Entries, nil
 }
